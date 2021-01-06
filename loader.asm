272378094fbff0ccb04cb8d38a2ad71e56600be5
Hussain-7 <hussain2000.rizvi@gmail.com>
2021-01-06 11:49:24 +0500

    Long mode support added

--- loader.asm
+++ loader.asm
@@ -6,10 +6,21 @@
 start:
     mov [DriveId],dl
     ;passing this value to eax will return processor feature when eax is passed as cpuid directive parameter
-    ;Info about long mode support is stored in edx if 29 in edx than long mode else not supported
+    ;Info about long mode support is stored in edx using test we check if bit 29 is set in edx then long mode else not supported
+    ;I gb page support check on bit 26 we test it using test function as well
+    mov eax,0x80000000
+    cpuid
+    cmp eax,0x80000001
+    jb NotSupport
+
     mov eax,0x80000001
+    cpuid
     test edx,(1<<29)
-    cpuid 
+    jz NotSupport
+
+    test edx,(1<<26)
+    jz NotSupport
+
     mov ah,0x13
     mov al,1
     mov bx,0xa
@@ -17,12 +28,12 @@ start:
     mov bp,Message
     mov cx,MessageLen 
     int 0x10
-    
+
+NotSupport:
 End:
     hlt
     jmp End
 
-
 DriveId:    db 0
-Message:    db "loader starts"
+Message:    db "long mode is supported"
 MessageLen: equ $-Message
