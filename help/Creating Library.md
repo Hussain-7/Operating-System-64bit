127e3ff32f29e1f9120c850331fb718c7e1bcc87
Hussain-7 <hussain2000.rizvi@gmail.com>
2021-01-14 02:08:48 +0500

    Done till printf using System call

--- help/Creating Library.md
+++ help/Creating Library.md
@@ -1,1 +1,5 @@
 ## Command : ar rcs lib.a print.o syscall.o ##
+
+
+## Purpose of linking in lib ##
+Just as we need linker script when building the kernel, we also need it building user program.
--- lib/start.asm
+++ lib/start.asm
@@ -2,6 +2,12 @@ section .text
 global start
 extern main
 
+;To compile the main file, we need a start file which prepare the environment for main function.
+;In the normal cases where we use c standard library, we don’t need to do this because it did it for us.
+;But in our system, we have to do everything ourselves.
+;After we get to ring3, the start is the first function being called and the stack is prepared when 
+;we set the process entry in the process module. So we simply call the main function.
+;jmp $ act as an infinite loop here we cannot use hlt since in user mode
 start:
     call main
     jmp $
